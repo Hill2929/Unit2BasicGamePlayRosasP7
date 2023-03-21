9a5f12095d890e098ba0d1034c8375d05d4cff41
Hill2929 <736120@student.dusd.net>
2023-03-21 15:30:07 -0700

    marco messed it up but fixed

--- Unit2BasicGamePlayP7Rosas/Assets/Scripts/MoveFoward.cs
+++ Unit2BasicGamePlayP7Rosas/Assets/Scripts/MoveFoward.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class MoveFoward : MonoBehaviour
 {
     public float speed = 40.00f;
-  
+    
 
     // Start is called before the first frame update
     void Start()
--- Unit2BasicGamePlayP7Rosas/Assets/Scripts/SpawnManager.cs
+++ Unit2BasicGamePlayP7Rosas/Assets/Scripts/SpawnManager.cs
@@ -20,18 +20,18 @@ public class SpawnManager : MonoBehaviour
     // Update is called once per frame
     void SpawnRandomAnimal()
     {
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX),
-            0, spawnPosZ); 
-            int animalIndex = Random.Range(0, animalPrefabs.Length);
-            Instantiate(animalPrefabs[animalIndex], spawnPos,
-            animalPrefabs[animalIndex].transform.rotation); 
-        
-        }
-
+    
+           
     }
 
-
-
+     void Update()
+     {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            // Randomly generate animal index and spawn position 
+            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+            int animalIndex = Random.Range(0, animalPrefabs.Length);
+            Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation); 
+        }
+     }
 }
